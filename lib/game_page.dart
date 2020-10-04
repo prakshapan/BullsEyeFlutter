79e2a9d647f6ba2ef5539e9682e62bd9a6c76630
Sagun Raj Lage <sagoonraj@gmail.com>
2020-10-04 08:32:41 -0500

    [add] adds basic button functionalities and alerts

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class GamePage extends StatefulWidget {
@@ -12,6 +13,8 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  bool _alertIsVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,10 +26,58 @@ class _GamePageState extends State<GamePage> {
             "Welcome to the app!",
             style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
           ),
-          FlatButton(
+          RaisedButton(
             child: Text("Hit me!", style: TextStyle(color: Colors.blue)),
-            onPressed: () {},
+            onPressed: () {
+              this._alertIsVisible = true;
+              this._showAlert(context);
+            },
+          ),
+          FlatButton(
+            child: Text("Knock Knock!", style: TextStyle(color: Colors.red)),
+            onPressed: () {
+              this._alertIsVisible = true;
+              this._showKnockKnockAlert(context);
+            },
           )
         ])));
+  }
+
+  void _showKnockKnockAlert(BuildContext context) {
+    Widget dismissButton = FlatButton(
+      child: Text("Dismiss"),
+      onPressed: () {
+        this._alertIsVisible = false;
+        Navigator.of(context).pop();
+      },
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            actions: [dismissButton],
+            title: Text("Who's there?"),
+            content: Text("Some random joke!"),
+          );
+        });
+  }
+
+  void _showAlert(BuildContext context) {
+    Widget okButton = FlatButton(
+        child: Text("Awesome"),
+        onPressed: () {
+          Navigator.of(context).pop();
+          this._alertIsVisible = false;
+        });
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Hello there!"),
+            content: Text("This is my first popup"),
+            actions: <Widget>[okButton],
+            elevation: 5,
+          );
+        });
   }
 }
