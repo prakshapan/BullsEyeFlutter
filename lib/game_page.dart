ddf8699e9b1a8ee4ac117a5652e88918b8d24221
Sagun Raj Lage <sagoonraj@gmail.com>
2020-10-07 07:30:55 -0500

    [add] adds background image

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -24,38 +24,51 @@ class _GamePageState extends State<GamePage> {
   @override
   void initState() {
     super.initState();
-    _model = GameModel(Random().nextInt(100) + 1);
+    _model = GameModel(_newTargetValue());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-          Prompt(targetValue: _model.target),
-          Control(
-            model: _model,
-          ),
-          RaisedButton(
-            child: Text("Hit me!", style: TextStyle(color: Colors.blue)),
-            onPressed: () {
-              this._alertIsVisible = true;
-              this._showAlert(context);
-            },
-          ),
-          Score(
-            round: _model.round,
-            totalScore: _model.totalScore,
-          )
-        ])));
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          image: DecorationImage(
+              image: AssetImage('images/background.png'),
+              fit: BoxFit.cover)),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                Prompt(targetValue: _model.target),
+                Control(
+                  model: _model,
+                ),
+                RaisedButton(
+                  child: Text("Hit me!", style: TextStyle(color: Colors.blue)),
+                  onPressed: () {
+                    this._showAlert(context);
+                  },
+                ),
+                Score(
+                    round: _model.round,
+                    totalScore: _model.totalScore,
+                    onStartOver: _startNewGame)
+              ]))),
+    );
   }
 
   int _pointsForCurrentRound() {
     var maximumScore = 100;
-    var difference = (_model.target - _model.current).abs();
-    return maximumScore - difference;
+    var difference = _amountOff();
+    var bonus = 0;
+    if (difference == 0) {
+      bonus = 100;
+    } else if (difference == 1) {
+      bonus = 50;
+    }
+    return maximumScore - difference + bonus;
   }
 
   void _showAlert(BuildContext context) {
@@ -64,16 +77,51 @@ class _GamePageState extends State<GamePage> {
         onPressed: () {
           Navigator.of(context).pop();
           this._alertIsVisible = false;
+          setState(() {
+            _model.totalScore += _pointsForCurrentRound();
+            _model.target = _newTargetValue();
+            _model.round++;
+          });
         });
     showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text("Hello there!"),
-            content: Text("The slider's value is ${_model.current}.\n You scored ${_pointsForCurrentRound()} this round."),
+            title: Text(_alertTitle()),
+            content: Text(
+                "The slider's value is ${_model.current}.\n You scored ${_pointsForCurrentRound()} this round."),
             actions: <Widget>[okButton],
             elevation: 5,
           );
         });
+  }
+
+  int _amountOff() => (_model.target - _model.current).abs();
+
+  String _alertTitle() {
+    var difference = _amountOff();
+    String title;
+    if (difference == 0) {
+      title = "Perfect!";
+    } else if (difference < 5) {
+      title = "You almost had it!";
+    } else if (difference <= 10) {
+      title = "Not bad!";
+    } else {
+      title = "Are you even trying?";
+    }
+
+    return title;
+  }
+
+  int _newTargetValue() => Random().nextInt(100) + 1;
+
+  void _startNewGame() {
+    setState(() {
+      _model.totalScore = GameModel.SCORE_START;
+      _model.round = GameModel.ROUND_START;
+      _model.target = _newTargetValue();
+      _model.current = GameModel.SLIDER_START;
+    });
   }
 }
